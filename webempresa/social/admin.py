9f93a75f80ee60f786f66da7e3e8d1e83f2849ca
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2019-09-24 22:30:09 -0300

    Custom social admin

--- webempresa/social/admin.py
+++ webempresa/social/admin.py
@@ -5,5 +5,11 @@ from webempresa.social.models import Link
 class LinkModelAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
 
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.groups.filter(name='Personal').exists():
+            return ('key', 'name')
+        else:
+            return ('created', 'updated')
+
 
 admin.site.register(Link, LinkModelAdmin)
